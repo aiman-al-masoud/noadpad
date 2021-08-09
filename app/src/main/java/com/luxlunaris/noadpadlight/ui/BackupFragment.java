033d535667b77fca27c4650076c9f32ace521302
aiman-al-masoud <luxlunarislabs@gmail.com>
2021-08-10 00:46:11 +0200

    Test push after repo name change.

--- app/src/main/java/com/luxlunaris/noadpadlight/ui/BackupFragment.java
+++ app/src/main/java/com/luxlunaris/noadpadlight/ui/BackupFragment.java
@@ -55,7 +55,7 @@ public class BackupFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment.
         View view  =  inflater.inflate(R.layout.fragment_backup, container, false);
 
         //get import and export buttons
